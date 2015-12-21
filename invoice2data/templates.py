66e280a7daf196f2843e394aade4c42a7dbb8f25
Alexis de Lattre <alexis.delattre@akretion.com>
2015-12-21 02:19:18 +0100

    Add Akretion in invoice template

--- invoice2data/templates.py
+++ invoice2data/templates.py
@@ -42,6 +42,9 @@ templates = [
                 {'keyword': 'FR58512277450',  # https://www.captaintrain.com/ TODO in progress
                  'data': [
                          ('invoice_number', r'Billet ([A-Z0-9]+)'),
+                         ('amount', r'(\d+,\d{2})'),
+                         ('amount_untaxed', r'(\d+,\d{2})'),
+#                         ('description', r'Billet [A-Z0-9]+ − ([^\s{3}]+)'),
                         ]
                 },
                 {'keyword': 'FR 71 343059564',  # http://www.sfr.fr/
@@ -52,6 +55,16 @@ templates = [
                          ('amount_untaxed', r'(\d+,\d{2}) € HT'),
                          ('description', r'(abonnements, forfaits et options du \d{2}/\d{2} au \d{2}/\d{2})'),
                          ('vat', r'(FR 71 343059564)'),
+                        ]
+                },
+                {'keyword': '792 377 731',  # http://www.akretion.com/
+                 'data': [
+                        ('amount', r'Total TTC :\s+(\d+,\d{2})'),
+                        ('amount_untaxed', r'Total HT :\s+(\d+,\d{2})'),
+                        ('invoice_number', r'Facture (\w+)'),
+                        ('date', r'(\d{2}/\d{2}/\d{4})'),
+                        ('date_due', r'\d{2}/\d{2}/\d{4}.+(\d{2}/\d{2}/\d{4})'),
+                        ('siren', r'(792 377 731)'),
                         ]
                 },
 ]
