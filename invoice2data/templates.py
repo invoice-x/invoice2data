8f008677ffdfe7c698428e4f2b8b944b5bffcc35
Alexis de Lattre <alexis.delattre@akretion.com>
2015-12-22 00:50:05 +0100

    Add SFR xDSL/fiber and Bouygues telecom

--- invoice2data/templates.py
+++ invoice2data/templates.py
@@ -40,13 +40,14 @@ templates = [
                 },
                 {'keyword': 'FR58512277450',  # https://www.captaintrain.com/ TODO in progress
                  'data': [
-                         ('invoice_number', r'Billet ([A-Z0-9]+)'),
+                         ('invoice_number', r'Billet ([A-Z]+)'),
                          ('amount', r'(\d+,\d{2})'),
                          ('amount_untaxed', r'(\d+,\d{2})'),
-#                         ('description', r'Billet [A-Z0-9]+ − ([^\s{3}]+)'),
+                         ('date', r'\d+,\d{2}\D+(\d+\s.+\s\d{4})'),
+                         ('description', r'Billet [A-Z]+ − ([^\s{5}])'),
                         ]
                 },
-                {'keyword': 'FR 71 343059564',  # http://www.sfr.fr/
+                {'keyword': 'FR 71 343059564',  # http://www.sfr.fr/ Mobile phone
                  'data': [
                          ('invoice_number', r'N° facture : ([A-Z0-9\-]+)'),
                          ('date', r'Date de facture : (\d{2}/\d{2}/\d{4})'),
@@ -54,6 +55,15 @@ templates = [
                          ('amount_untaxed', r'(\d+,\d{2}) € HT'),
                          ('description', r'(abonnements, forfaits et options du \d{2}/\d{2} au \d{2}/\d{2})'),
                          ('vat', r'(FR 71 343059564)'),
+                        ]
+                },
+                {'keyword': 'FR 71 343 059 564',  # http://www.sfr.fr/ xDSL/Fiber access
+                 'data': [
+                         ('invoice_number', r'N° de Facture\s:\s(\d+)'),
+                         ('date', r'Facture du (\d{2}/\d{2}/\d{4})'),
+                         ('amount', r'Total facturé.+\d+,\d{2}\s+\d+,\d{2}\s+(\d+,\d{2})'),
+                         ('amount_untaxed', r'Total facturé.+(\d+,\d{2})'),
+                         ('vat', r'(FR 71 343 059 564)'),
                         ]
                 },
                 {'keyword': '792 377 731',  # http://www.akretion.com/
@@ -83,6 +93,15 @@ templates = [
                     ('date', r'Facture n°\d+ du (\d+ .+ \d{4})'),
                     ('date_due', r'Date limite de paiement le (\d+ .+ \d{4})'),
                     ('invoice_number', r'Facture n°(\d+)'),
+                    ]
+                },
+                {'keyword': 'FR 74 397 480 930',  # http://www.bouyguestelecom.fr/
+                'data': [
+                    ('vat', r'(FR 74 397 480 930)'),
+                    ('amount_untaxed', r'Montant de la facture soumis à TVA\s+(\d+,\d{2})'),
+                    ('amount', r'Montant de la facture soumis à TVA\s+\d+,\d{2}\s+(\d+,\d{2})'),
+                    ('date', r'Date de facture\s+:\s+(\d{2}/\d{2}/\d{4})'),
+                    ('invoice_number', r'N° de facture\s+:\s+(\d+)'),
                     ]
                 },
 ]
