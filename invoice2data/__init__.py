fa01b87449810f7556945a626e7c24bc14c9126e
Sébastien BEAU <sebastien.beau@akretion.com>
2015-12-24 14:36:28 +0100

    [IMP] add setup.py and update readme closes #2

---
+++ invoice2data/__init__.py
@@ -0,0 +1,1 @@
+from .main import extract_data
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+version = open('VERSION').read().strip()
+
+setup(
+    name='invoice2data',
+    version=version,
+    author='Manuel Riel',
+    author_email='which email should we use?',
+    url='https://github.com/manuelRiel/invoice2data',
+    description='Python parser for extracting data from pdf invoice',
+    license="We need to define a licence",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[ r.strip() for r in open('requirements.txt').read().splitlines() ],
+    zip_safe=False
+)
