235d82b63e61e53e9983ad954c8ed1dcf732be16
Manu <manu@snapdragon.cc>
2016-04-02 23:43:42 +0800

    Bump version: 0.2.9 → 0.2.10

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ elif sys.version_info[0] == 3:
 
 setup(
     name='invoice2data',
-    version='0.2.9',
+    version='0.2.10',
     author='Manuel Riel',
     author_email='github@snapdragon.cc',
     url='https://github.com/m3nu/invoice2data',
