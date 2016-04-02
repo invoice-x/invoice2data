9d85b97d51b26e4728b0e247b5a1f9d036eb59e9
Manu <manu@snapdragon.cc>
2016-04-02 21:59:07 +0800

    Use tox for testing.

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-version = open('VERSION').read().strip()
+from os import path
 
 setup(
     name='invoice2data',
-    version=version,
+    version='0.2.5',
     author='Manuel Riel',
     author_email='github@snapdragon.cc',
     url='https://github.com/manuelRiel/invoice2data',
     description='Python parser to extract data from pdf invoice',
     license="MIT",
-    long_description=open('README.md').read(),
+    long_description=open(path.join(path.dirname(__file__), 'README.md')).read(),
     package_data = {
         'invoice2data': ['templates/*.yml', 'templates/fr/*.yml'],
         'invoice2data.test': ['pdfs/*.pdf']
         },
     packages=find_packages(),
     install_requires=[
-        r.strip() for r in open('requirements.txt').read().splitlines()],
+        r.strip() for r in open(path.join(path.dirname(__file__), 'requirements.txt')).read().splitlines()],
     zip_safe=False,
     entry_points = {
               'console_scripts': [
