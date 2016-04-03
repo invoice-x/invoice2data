f6f24bb732be14c588a5acf7a0f4d2927f98a9e1
Manu <manu@snapdragon.cc>
2016-04-03 13:53:49 +0800

    Use py2+3 compatible pdfminer fork.

--- setup.py
+++ setup.py
@@ -3,14 +3,9 @@ from setuptools import setup, find_packages
 from os import path
 import sys
 
-if sys.version_info[0] == 2:
-    pdfminer = "pdfminer"
-elif sys.version_info[0] == 3:
-    pdfminer = "pdfminer3k"
-
 setup(
     name='invoice2data',
-    version='0.2.12',
+    version='0.2.13',
     author='Manuel Riel',
     author_email='github@snapdragon.cc',
     url='https://github.com/m3nu/invoice2data',
@@ -22,12 +17,12 @@ setup(
         'invoice2data.test': ['pdfs/*.pdf']
         },
     packages=find_packages(),
-    install_requires=[pdfminer] + 
-        [r.strip() for r in open(path.join(path.dirname(__file__), 'requirements.txt')).read().splitlines()],
+    install_requires=[
+        r.strip() for r in open(path.join(path.dirname(__file__), 'requirements.txt')).read().splitlines()],
     zip_safe=False,
     entry_points = {
               'console_scripts': [
-                  'invoice2data = invoice2data.main:main',                  
-              ],              
+                  'invoice2data = invoice2data.main:main',
+              ],
           },
 )
