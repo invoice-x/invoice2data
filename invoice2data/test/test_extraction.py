33e90892228d787ed73e3560c5a3bda0ece70f5c
Manu <manu@snapdragon.cc>
2016-05-14 15:36:05 +0800

    properly push tags?

--- invoice2data/test/test_extraction.py
+++ invoice2data/test/test_extraction.py
@@ -27,7 +27,8 @@ class TestExtraction(unittest.TestCase):
     def _run_test_on_folder(self, folder):
         for path, subdirs, files in os.walk(folder):
             for file in files:
-                extract_data(os.path.join(path, file), self.templates)            
+                res = extract_data(os.path.join(path, file), self.templates)   
+                print(file, res)         
 
     def test_external_pdfs(self):
         folder = os.getenv('EXTERNAL_PDFS', None)
