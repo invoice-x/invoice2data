a1fab6d2868977fe31a3ee59dc8eb5e2479d22ef
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-09-25 11:01:30 +0200

    Add condition endswith for dates remove lambda function, unnecessary

--- invoice2data/template.py
+++ invoice2data/template.py
@@ -27,9 +27,6 @@ OPTIONS_DEFAULT = {
     'line_separator': r'\n',
 }
 
-# add to this lambda if you want more fields parsed as date field
-is_date_field = lambda x: x.startswith('date') or x == 'requested_date'
-
 def read_templates(folder):
     """
     Load yaml templates from template folder. Return list of dicts.
@@ -181,7 +178,7 @@ class InvoiceTemplate(OrderedDict):
                     res_find = re.findall(v, optimized_str)
                 if res_find:
                     logger.debug("res_find=%s", res_find)
-                    if is_date_field(k):
+                    if k.startswith('date') or k.endswith('date'):
                         output[k] = self.parse_date(res_find[0])
                         if not output[k]:
                             logger.error(
