e7e018aaf5f06e5cda93d87b1d9c4a62df4b2511
Udit Juneja <34393070+uditjuneja1@users.noreply.github.com>
2018-04-11 11:45:47 +0530

    Update main.py

--- invoice2data_gui/main.py
+++ invoice2data_gui/main.py
@@ -1,24 +1,105 @@
+import csv
+import json
+import getpass
+from os.path import join
+from tkinter.filedialog import askopenfilename
+from tkinter import StringVar
 from tkinter import Tk
 from tkinter import ttk
-from tkinter.filedialog import askopenfilename
+from dicttoxml import dicttoxml
 from invoice2data import extract_data
-
 
 def draw_gui():
     root = Tk()
     root.title("invoice2data GUI")
     root.resizable(False, False)
-
-    input_label = ttk.Label(root, text="Invoice File:")
-    input_entry = ttk.Entry(root, width=20)
-    input_ent_but = ttk.Button(root, text="Select")
-
+#Function to take input of file
     def input_path_com():
         Tk().withdraw()
         filename = askopenfilename()
         input_entry.delete(0, "")
         input_entry.insert(0, filename)
+#Function to process data and save it on Desktop
+    def submit_com():
+        filename = input_entry.get()
+        status_entry.delete(0, "")
+        status_entry.insert(0, "Cheking for file")
 
+        content = extract_data(filename)
+
+        if str(content) == "False":
+            status_entry.delete(0, "")
+            status_entry.insert(0, "File Error!!!")
+
+        else:
+            file_type = list_option.get()
+            file_type = file_type.lower()
+            file_name = "output." + file_type
+
+            if file_type == "xml":
+                content = dicttoxml(content, custom_root='test', attr_type=False)
+
+                try:
+                    username = getpass.getuser()
+                    path = "/home/" + username + "/Desktop"
+                    out_file_open = open(join(path, file_name), 'w')
+                    out_file_open.write(str(content))
+                    out_file_open.close()
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, "Check Desktop")
+                    input_entry.delete(0, "")
+
+                except:
+                    out_file_open = open((file_name), 'w')
+                    out_file_open.write(str(content))
+                    out_file_open.close()
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, ("Successfully written in " + file_name))
+                    input_entry.delete(0, "")
+
+            if file_type == "csv":
+                try:
+                    username = getpass.getuser()
+                    path = "/home/" + username + "/Desktop"
+                    with open(join(path, file_name), 'wb') as file_opner:  # Just use 'w' mode in 3.x
+                        file_csv = csv.DictWriter(file_opner, content.keys())
+                        file_csv.writeheader()
+                        file_csv.writerow(content)
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, "Check Desktop")
+                    input_entry.delete(0, "")
+
+                except:
+                    with open((file_name), 'w') as file_opner:  # Just use 'w' mode in 3.x
+                        file_csv = csv.DictWriter(file_opner, content.keys())
+                        file_csv.writeheader()
+                        file_csv.writerow(content)
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, ("Successfully written in " + file_name))
+                    input_entry.delete(0, "")
+
+            if file_type == "json":
+                try:
+                    username = getpass.getuser()
+                    path = "/home/" + username + "/Desktop"
+                    out_file_open = open(join(path, file_name), 'w')
+                    out_file_open.write(str(content))
+                    out_file_open.close()
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, "Check Desktop")
+                    input_entry.delete(0, "")
+
+                except:
+                    out_file_open = open((file_name), 'w')
+                    out_file_open.write(str(content))
+                    out_file_open.close()
+                    status_entry.delete(0, "")
+                    status_entry.insert(0, ("Successfully written in " + file_name))
+                    input_entry.delete(0, "")
+#GUI started
+    input_label = ttk.Label(root, text="Invoice File:")
+    input_entry = ttk.Entry(root, width=20)
+    input_ent_but = ttk.Button(root, text="Select")
     input_ent_but.config(command=input_path_com)
     input_label.grid(row=0, column=0)
     input_entry.grid(row=0, column=1)
@@ -28,36 +109,16 @@ def draw_gui():
     sample1_label.grid(row=1, column=0)
 
     output_label = ttk.Label(root, text="Output Format")
-    output_type_label = ttk.Label(root, text="JSON")
+    list_option = StringVar(root)
+    list_option.set("JSON")
+    output_type = ttk.OptionMenu(root, list_option, 'JSON', 'JSON', 'CSV', 'XML')
     output_label.grid(row=2, column=0)
-    output_type_label.grid(row=2, column=1)
+    output_type.grid(row=2, column=1)
 
     sample2_label = ttk.Label(root, text=" ")
     sample2_label.grid(row=3, column=0)
 
     submit_but = ttk.Button(root, text="Extract")
-
-    def submit_com():
-        filename = input_entry.get()
-        status_entry.delete(0, "")
-        status_entry.insert(0, "Cheking for file")
-
-        try:
-            result = extract_data(filename)
-            status_entry.delete(0, "")
-            status_entry.insert(0, "File Found")
-        except:
-            status_entry.delete(0, "")
-            status_entry.insert(0, "File Error!!!")
-
-        out_file_open = open('output.json', 'w')
-        out_file_open.write(str(result))
-        out_file_open.close()
-
-        status_entry.delete(0, "")
-        status_entry.insert(0, "Successfully extracted to 'output.json'")
-        input_entry.delete(0, "END")
-
     submit_but.config(command=submit_com)
     submit_but.grid(row=4, column=1)
 
@@ -70,7 +131,6 @@ def draw_gui():
     status_entry.grid(row=7, column=1)
 
     root.mainloop()
-
 
 def main():
     draw_gui()
