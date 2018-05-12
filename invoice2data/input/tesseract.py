165ccd4abfadb5a42d029b4aa4cdbb6651a3d7e5
duskybomb <harshit.joshi@outlook.com>
2018-05-13 00:03:59 +0530

    quick fix for better msg when tesseract dependencies missing

--- invoice2data/input/tesseract.py
+++ invoice2data/input/tesseract.py
@@ -3,13 +3,17 @@ def to_text(path):
     """Wraps Tesseract OCR."""
     import subprocess
 
-    convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
-    p1 = subprocess.Popen(convert.split(' '), stdout=subprocess.PIPE)
+    try:
+        convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
+        p1 = subprocess.Popen(convert.split(' '), stdout=subprocess.PIPE)
 
-    p2 = subprocess.Popen("tesseract stdin stdout".split(' '),
-                          stdin=p1.stdout, stdout=subprocess.PIPE)
-    out, err = p2.communicate()
+        p2 = subprocess.Popen("tesseract stdin stdout".split(' '),
+                              stdin=p1.stdout, stdout=subprocess.PIPE)
+        out, err = p2.communicate()
 
-    extracted_str = out
+        extracted_str = out
 
-    return extracted_str
+        return extracted_str
+    except:
+        raise EnvironmentError('Make sure all pre-requisite libraries are installed')
+
