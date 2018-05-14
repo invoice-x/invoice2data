dc11024e553b8bc16eed36c4de342cebefc56f2c
Harshit Joshi <harshit.joshi@outlook.com>
2018-05-14 14:49:10 +0530

    Datetime fix (#121)

--- invoice2data/output/to_json.py
+++ invoice2data/output/to_json.py
@@ -1,5 +1,9 @@
 import json
+import datetime
 
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 def write_to_file(data, path):
     if path.endswith('.json'):
@@ -10,4 +14,4 @@ def write_to_file(data, path):
     with open(filename, "w") as json_file:
         for line in data:
             line['date'] = line['date'].strftime('%d/%m/%Y')
-        json.dump(data, json_file, indent=4,  sort_keys=True)
+        json.dump(data, json_file, indent=4,  sort_keys=True, default = myconverter)
