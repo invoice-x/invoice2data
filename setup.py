f6c163916de24ef54a8ddaa0df54cfd70cde4149
Manuel <manu@snapdragon.cc>
2018-05-15 06:20:38 +0000

    Bump version 0.2.86 > 0.2.87 [skip ci]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='invoice2data',
-    version='0.2.86',
+    version='0.2.87',
     author='Manuel Riel',
     author_email='github@snapdragon.cc',
     url='https://github.com/m3nu/invoice2data',
