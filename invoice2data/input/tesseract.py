b6c647ec715439feadf8752842364e38928c9097
Manu <manu@snapdragon.cc>
2018-05-21 11:34:36 +0800

    Move dependency checks for tesseract to top of module.

--- invoice2data/input/tesseract.py
+++ invoice2data/input/tesseract.py
@@ -16,19 +16,19 @@ def to_text(path):
     import subprocess
     from distutils import spawn
 
-    if spawn.find_executable('tesseract') and spawn.find_executable('convert'):
-        convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
-        p1 = subprocess.Popen(convert.split(' '), stdout=subprocess.PIPE)
+    # Check for dependencies. Needs Tesseract and Imagemagick installed.
+    if not spawn.find_executable('tesseract'):
+        raise EnvironmentError('tesseract not installed.')
+    if not spawn.find_executable('convert'):
+        raise EnvironmentError('imagemagick not installed.')
 
-        p2 = subprocess.Popen("tesseract stdin stdout".split(' '),
-                              stdin=p1.stdout, stdout=subprocess.PIPE)
-        out, err = p2.communicate()
+    convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
+    p1 = subprocess.Popen(convert.split(' '), stdout=subprocess.PIPE)
 
-        extracted_str = out
+    p2 = subprocess.Popen("tesseract stdin stdout".split(' '),
+                          stdin=p1.stdout, stdout=subprocess.PIPE)
+    out, err = p2.communicate()
 
-        return extracted_str
-    else:
-        if not spawn.find_executable('tesseract'):
-            raise EnvironmentError('tesseract not installed.')
-        if not spawn.find_executable('convert'):
-            raise EnvironmentError('imagemagick not installed.')
+    extracted_str = out
+
+    return extracted_str
