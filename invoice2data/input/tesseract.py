a3653295d75da194197f0a8cee5204f5df7c5268
duskybomb <harshit113@ducic.ac.in>
2018-05-24 11:26:08 +0530

    Remove argument as string for subprocess and use list instead.

--- invoice2data/input/tesseract.py
+++ invoice2data/input/tesseract.py
@@ -22,11 +22,13 @@ def to_text(path):
     if not spawn.find_executable('convert'):
         raise EnvironmentError('imagemagick not installed.')
 
-    convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
-    p1 = subprocess.Popen(convert.split(' '), stdout=subprocess.PIPE)
+    # convert = "convert -density 350 %s -depth 8 tiff:-" % (path)
+    convert = ['convert', '-density', '350', path, '-depth', '8', 'tiff:-']
+    p1 = subprocess.Popen(convert, stdout=subprocess.PIPE)
 
-    p2 = subprocess.Popen("tesseract stdin stdout".split(' '),
-                          stdin=p1.stdout, stdout=subprocess.PIPE)
+    tess = ['tesseract', 'stdin', 'stdout']
+    p2 = subprocess.Popen(tess, stdin=p1.stdout, stdout=subprocess.PIPE)
+
     out, err = p2.communicate()
 
     extracted_str = out
