852dc5f5bd58ce45826a04c025978c90d1693037
duskybomb <harshit.joshi@outlook.com>
2018-08-16 22:16:56 +0530

    Fixes 157, allow non ascii characters

--- invoice2data/output/to_json.py
+++ invoice2data/output/to_json.py
@@ -39,4 +39,4 @@ def write_to_file(data, path):
     with open(filename, "w") as json_file:
         for line in data:
             line['date'] = line['date'].strftime('%d/%m/%Y')
-        json.dump(data, json_file, indent=4,  sort_keys=True, default = myconverter)
+        json.dump(data, json_file, indent=4,  sort_keys=True, default = myconverter, ensure_ascii=False)
