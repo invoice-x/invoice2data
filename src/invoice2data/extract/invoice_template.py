b55ee272ba2517252e6cd9143bc6a0622d7c2123
Sam Slater <sam.slater@cces.local>
2019-09-03 09:51:28 +0100

    template fix

--- src/invoice2data/extract/invoice_template.py
+++ src/invoice2data/extract/invoice_template.py
@@ -4,100 +4,229 @@ This module abstracts templates for invoice providers.
 Templates are initially read from .yml files and then kept as class.
 """
 
-import os
-import yaml
-import pkg_resources
-from collections import OrderedDict
+import re
+import dateparser
+from unidecode import unidecode
 import logging
-from .invoice_template import InvoiceTemplate
-import codecs
-import chardet
+from collections import OrderedDict
+from .plugins import lines, tables, chunks
 
-logging.getLogger("chardet").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+
+OPTIONS_DEFAULT = {
+    "remove_whitespace": False,
+    "remove_accents": False,
+    "lowercase": False,
+    "currency": "EUR",
+    "date_formats": [],
+    "languages": [],
+    "decimal_separator": ".",
+    "replace": [],  # example: see templates/fr/fr.free.mobile.yml
+}
+
+PLUGIN_MAPPING = {"lines": lines, "tables": tables, "chunks": chunks}
 
 
-# borrowed from http://stackoverflow.com/a/21912744
-def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-    """load mappings and ordered mappings
-
-    loader to load mappings and ordered mappings into the Python 2.7+ OrderedDict type,
-    instead of the vanilla dict and the list of pairs it currently uses.
+class InvoiceTemplate(OrderedDict):
     """
+    Represents single template files that live as .yml files on the disk.
 
-    class OrderedLoader(Loader):
-        pass
-
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
-
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping
-    )
-
-    return yaml.load(stream, OrderedLoader)
-
-
-def read_templates(folder=None):
-    """
-    Load yaml templates from template folder. Return list of dicts.
-
-    Use built-in templates if no folder is set.
-
-    Parameters
-    ----------
-    folder : str
-        user defined folder where they stores their files, if None uses built-in templates
-
-    Returns
+    Methods
     -------
-    output : Instance of `InvoiceTemplate`
-        template which match based on keywords
-
-    Examples
-    --------
-
-    >>> read_template("home/duskybomb/invoice-templates/")
-    InvoiceTemplate([('issuer', 'OYO'), ('fields', OrderedDict([('amount', 'GrandTotalRs(\\d+)'),
-    ('date', 'Date:(\\d{1,2}\\/\\d{1,2}\\/\\d{1,4})'), ('invoice_number', '([A-Z0-9]+)CashatHotel')])),
-    ('keywords', ['OYO', 'Oravel', 'Stays']), ('options', OrderedDict([('currency', 'INR'), ('decimal_separator', '.'),
-    ('remove_whitespace', True)])), ('template_name', 'com.oyo.invoice.yml')])
-
-    After reading the template you can use the result as an instance of `InvoiceTemplate` to extract fields from
-    `extract_data()`
-
-    >>> my_template = InvoiceTemplate([('issuer', 'OYO'), ('fields', OrderedDict([('amount', 'GrandTotalRs(\\d+)'),
-    ('date', 'Date:(\\d{1,2}\\/\\d{1,2}\\/\\d{1,4})'), ('invoice_number', '([A-Z0-9]+)CashatHotel')])),
-    ('keywords', ['OYO', 'Oravel', 'Stays']), ('options', OrderedDict([('currency', 'INR'), ('decimal_separator', '.'),
-    ('remove_whitespace', True)])), ('template_name', 'com.oyo.invoice.yml')])
-    >>> extract_data(iinvoice2dataNewew, my_template, pdftotext)
-    {'issuer': 'OYO', 'amount': 1939.0, 'date': datetime.datetime(2017, 12, 31, 0, 0), 'invoice_number': 'IBZY2087',
-     'currency': 'INR', 'desc': 'Invoice IBZY2087 from OYO'}
-
+    prepare_input(extracted_str)
+        Input raw string and do transformations, as set in template file.
+    matches_input(optimized_str)
+        See if string matches keywords set in template file
+    parse_number(value)
+        Parse number, remove decimal separator and add other options
+    parse_date(value)
+        Parses date and returns date after parsing
+    coerce_type(value, target_type)
+        change type of values
+    extract(optimized_str)
+        Given a template file and a string, extract matching data fields.
     """
 
-    output = []
+    def __init__(self, *args, **kwargs):
+        super(InvoiceTemplate, self).__init__(*args, **kwargs)
 
-    if folder is None:
-        folder = pkg_resources.resource_filename(__name__, "templates")
+        # Merge template-specific options with defaults
+        self.options = OPTIONS_DEFAULT.copy()
 
-    for path, subdirs, files in os.walk(folder):
-        for name in sorted(files):
-            if name.endswith(".yml"):
-                with open(os.path.join(path, name), "rb") as f:
-                    encoding = chardet.detect(f.read())["encoding"]
-                with codecs.open(
-                    os.path.join(path, name), encoding=encoding
-                ) as template_file:
-                    tpl = ordered_load(template_file.read())
-                tpl["template_name"] = name
+        for lang in self.options["languages"]:
+            assert len(lang) == 2, "lang code must have 2 letters"
 
-                # Test if all required fields are in template:
-                assert "keywords" in tpl.keys(), "Missing keywords field."
+        if "options" in self:
+            self.options.update(self["options"])
 
-                # Keywords as list, if only one.
-                if type(tpl["keywords"]) is not list:
-                    tpl["keywords"] = [tpl["keywords"]]
+        # Set issuer, if it doesn't exist.
+        if "issuer" not in self.keys():
+            self["issuer"] = self["keywords"][0]
 
-                output.append(InvoiceTemplate(tpl))
-    return output
+    def prepare_input(self, extracted_str):
+        """
+        Input raw string and do transformations, as set in template file.
+        """
+
+        # Remove withspace
+        if self.options["remove_whitespace"]:
+            optimized_str = re.sub(" +", "", extracted_str)
+        else:
+            optimized_str = extracted_str
+
+        # Remove accents
+        if self.options["remove_accents"]:
+            optimized_str = unidecode(optimized_str)
+
+        # convert to lower case
+        if self.options["lowercase"]:
+            optimized_str = optimized_str.lower()
+
+        # specific replace
+        for replace in self.options["replace"]:
+            assert len(replace) == 2, "A replace should be a list of 2 items"
+            optimized_str = optimized_str.replace(replace[0], replace[1])
+
+        return optimized_str
+
+    def matches_input(self, optimized_str):
+        """See if string matches keywords set in template file"""
+
+        if all([keyword in optimized_str for keyword in self["keywords"]]):
+            logger.debug("Matched template %s", self["template_name"])
+            return True
+
+    def parse_number(self, value):
+        assert (
+            value.count(self.options["decimal_separator"]) < 2
+        ), "Decimal separator cannot be present several times"
+        # replace decimal separator by a |
+        amount_pipe = value.replace(self.options["decimal_separator"], "|")
+        # remove all possible thousands separators
+        amount_pipe_no_thousand_sep = re.sub(r"[.,\s]", "", amount_pipe)
+        # put dot as decimal sep
+        return float(amount_pipe_no_thousand_sep.replace("|", "."))
+
+    def parse_date(self, value):
+        """Parses date and returns date after parsing"""
+        res = dateparser.parse(
+            value,
+            date_formats=self.options["date_formats"],
+            languages=self.options["languages"],
+        )
+        logger.debug("result of date parsing=%s", res)
+        return res
+
+    def coerce_type(self, value, target_type):
+        if target_type == "int":
+            if not value.strip():
+                return 0
+            return int(self.parse_number(value))
+        elif target_type == "float":
+            if not value.strip():
+                return 0.0
+            return float(self.parse_number(value))
+        elif target_type == "date":
+            return self.parse_date(value)
+        assert False, "Unknown type"
+
+    def extract(self, optimized_str):
+        """
+        Given a template file and a string, extract matching data fields.
+        """
+
+        logger.debug("START optimized_str ========================")
+        logger.debug(optimized_str)
+        logger.debug("END optimized_str ==========================")
+        logger.debug(
+            "Date parsing: languages=%s date_formats=%s",
+            self.options["languages"],
+            self.options["date_formats"],
+        )
+        logger.debug(
+            "Float parsing: decimal separator=%s", self.options["decimal_separator"]
+        )
+        logger.debug("keywords=%s", self["keywords"])
+        logger.debug(self.options)
+
+        # Try to find data for each field.
+        output = {}
+        output["issuer"] = self["issuer"]
+
+        for k, v in self["fields"].items():
+            if k.startswith("static_"):
+                logger.debug("field=%s | static value=%s", k, v)
+                output[k.replace("static_", "")] = v
+            else:
+                logger.debug("field=%s | regexp=%s", k, v)
+
+                sum_field = False
+                if k.startswith("sum_amount") and type(v) is list:
+                    k = k[4:]  # remove 'sum_' prefix
+                    sum_field = True
+                # Fields can have multiple expressions
+                if type(v) is list:
+                    res_find = []
+                    for v_option in v:
+                        res_val = re.findall(v_option, optimized_str)
+                        if res_val:
+                            if sum_field:
+                                res_find += res_val
+                            else:
+                                res_find.extend(res_val)
+                else:
+                    res_find = re.findall(v, optimized_str)
+                if res_find:
+                    logger.debug("res_find=%s", res_find)
+                    if k.startswith("date") or k.endswith("date"):
+                        output[k] = self.parse_date(res_find[0])
+                        if not output[k]:
+                            logger.error(
+                                "Date parsing failed on date '%s'", res_find[0]
+                            )
+                            return None
+                    elif k.startswith("amount"):
+                        if sum_field:
+                            output[k] = 0
+                            for amount_to_parse in res_find:
+                                output[k] += self.parse_number(amount_to_parse)
+                        else:
+                            output[k] = self.parse_number(res_find[0])
+                    else:
+                        res_find = list(set(res_find))
+                        if len(res_find) == 1:
+                            output[k] = res_find[0]
+                        else:
+                            output[k] = res_find
+                else:
+                    logger.warning("regexp for field %s didn't match", k)
+
+        output["currency"] = self.options["currency"]
+
+        # Run plugins:
+        for plugin_keyword, plugin_func in PLUGIN_MAPPING.items():
+            if plugin_keyword in self.keys():
+                plugin_func.extract(self, optimized_str, output)
+
+        # If required fields were found, return output, else log error.
+        if "required_fields" not in self.keys():
+            required_fields = ["date", "amount", "invoice_number", "issuer"]
+        else:
+            required_fields = []
+            for v in self["required_fields"]:
+                required_fields.append(v)
+
+        if set(required_fields).issubset(output.keys()):
+            output["desc"] = "Invoice from %s" % (self["issuer"])
+            logger.debug(output)
+            return output
+        else:
+            fields = list(set(output.keys()))
+            logger.error(
+                "Unable to match all required fields. "
+                "The required fields are: {0}. "
+                "Output contains the following fields: {1}.".format(
+                    required_fields, fields
+                )
+            )
+            return None
