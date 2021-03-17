0da2ea77ac1af3a282cd1fa0465670384ba153cc
Ross Kukard <ross.kukard@perkinelmer.com>
2021-03-16 21:29:55 -0700

    Added area support for fields
    Fields specifying a parser now also accept an "area" argument. This allows one to specify the page(s) and coordinates of the area to be extracted and then applies the specified parser to the results as normal :)
    Currently this only works for pdftotext.
    Incorporated changes into all existing functions where possible to allow for ease of future maintainability.
    Tutorial was updated to show new option and its syntax.

--- src/invoice2data/input/pdftotext.py
+++ src/invoice2data/input/pdftotext.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
-def to_text(path):
+def to_text(path: str, area_details: dict=None):
     """Wrapper around Poppler pdftotext.
 
     Parameters
     ----------
     path : str
         path of electronic invoice in PDF
+    area_details : dictionary
+        of the format {x: int, y: int, r: int, W: int, H: int}
+        used when extracting an area of the pdf rather than the whole document
 
     Returns
     -------
@@ -21,9 +24,38 @@ def to_text(path):
     from distutils import spawn  # py2 compat
 
     if spawn.find_executable("pdftotext"):  # shutil.which('pdftotext'):
-        out, err = subprocess.Popen(
-            ["pdftotext", "-layout", "-enc", "UTF-8", path, "-"], stdout=subprocess.PIPE
-        ).communicate()
+        if area_details is not None:
+            # An area was specified
+            # Validate the required keys were provided
+            assert 'f' in area_details, 'Area r details missing'
+            assert 'l' in area_details, 'Area r details missing'
+            assert 'r' in area_details, 'Area r details missing'
+            assert 'x' in area_details, 'Area x details missing'
+            assert 'y' in area_details, 'Area y details missing'
+            assert 'W' in area_details, 'Area W details missing'
+            assert 'H' in area_details, 'Area H details missing'
+            # Convert all of the values to strings
+            for key in area_details.keys():
+                area_details[key] = str(area_details[key])
+            # Extract the data for the specified region
+            out, err = subprocess.Popen(
+                ["pdftotext","-layout", "-enc", "UTF-8", 
+                '-f',area_details['f'], 
+                '-l',area_details['l'], 
+                '-r',area_details['r'], 
+                '-x',area_details['x'], 
+                '-y',area_details['y'], 
+                '-W',area_details['W'], 
+                '-H',area_details['H'], 
+                path, '-'],
+                stdout=subprocess.PIPE
+            ).communicate()
+        else:
+            # No area was specified, so extract the entire pdf
+            out, err = subprocess.Popen(
+                ["pdftotext", "-layout", "-enc", "UTF-8", path, "-"],
+                stdout=subprocess.PIPE
+            ).communicate()
         return out
     else:
         raise EnvironmentError(
