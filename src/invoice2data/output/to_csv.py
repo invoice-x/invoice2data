2dbb723941cc142e183815085f904c5f565c1043
RossK1 <ross.kukard@perkinelmer.com>
2021-03-18 16:22:59 -0700

    My bad, its just a documentation hiccup
    The data argument to this function is actually a list, not a dictionary. If only a single file is being processed, that result must be passed to the function as a list.

--- src/invoice2data/output/to_csv.py
+++ src/invoice2data/output/to_csv.py
@@ -2,17 +2,18 @@ import csv
 import sys
 
 
-def write_to_file(data, path, date_format="%Y-%m-%d"):
+def write_to_file(data: list, path: str, date_format="%Y-%m-%d") -> None:
     """Export extracted fields to csv
 
     Appends .csv to path if missing and generates csv file in specified directory, if not then in root
 
     Parameters
     ----------
-    data : dict
-        Dictionary of extracted fields
+    data : list
+        A list of dictionaries of extracted fields.
+        If only a single file was processed, it must be passed as a single-element list.
     path : str
-        directory to save generated csv file
+        csv file to save output csv to
     date_format : str
         Date format used in generated file
 
@@ -40,29 +41,17 @@ def write_to_file(data, path, date_format="%Y-%m-%d"):
     with openfile as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
 
-        # Write header to csv
-        writer.writerow(data.keys())
+        for line in data:
+            first_row = []
+            for k, v in line.items():
+                first_row.append(k)
 
-        # Loop through data and see which had lists of results (e.g. from lines parser)
-        csv_items = []
-        for key in data.keys():
-            value = data[key]
-            if isinstance(value, list):
-                # List of results, process each in turn
-                result_list = []
-                for item in value:
-                    if isinstance(item, dict):
-                        # The item in the list is a dictionary, so parse each key-value pair for datetime formatting
-                        for sub_key in item.keys():
-                            sub_value = item[sub_key]
-                            if sub_key.startswith("date") or sub_key.endswith("date"):
-                                # Key appears to indicate a date, so parse it as such
-                                sub_value = sub_value.strftime(date_format)
-                    result_list.append(item)
-                csv_items.append(result_list)
-            else:
-                # Single value, so parse as date if necessary
-                if key.startswith("date") or key.endswith("date"):
-                    value = value.strftime(date_format)
-                csv_items.append(value)
-        writer.writerow(csv_items)
+        writer.writerow(first_row)
+        for line in data:
+            csv_items = []
+            for k, v in line.items():
+                # first_row.append(k)
+                if k.startswith("date") or k.endswith("date"):
+                    v = v.strftime(date_format)
+                csv_items.append(v)
+            writer.writerow(csv_items)
