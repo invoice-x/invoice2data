b6d625be199ec5ae2fe8430f11e2f7305f962557
Timo Rieber <trieber@texperience.de>
2023-02-19 12:34:53 +0100

    Refactor implementation using pathlib

--- src/invoice2data/extract/loader.py
+++ src/invoice2data/extract/loader.py
@@ -1,7 +1,7 @@
 import codecs
 import logging
-import os
 from collections import OrderedDict
+from pathlib import Path
 
 import pkg_resources
 import yaml
@@ -27,7 +27,7 @@ def ordered_load(stream):
     return yaml.load(stream, OrderedLoader)
 
 
-def read_templates(folder=None):
+def read_templates(folder: str = None):
     """
     Load yaml templates from template folder. Use built-in templates if no folder is set.
 
@@ -45,39 +45,35 @@ def read_templates(folder=None):
     if folder is None:
         folder = pkg_resources.resource_filename(__name__, "templates")
 
-    for path, _, files in os.walk(folder):
-        for name in sorted(files):
-            if name.endswith(".yml") is False:
+    templatedirectory: Path = Path(folder)
+
+    for yamlfile in sorted(templatedirectory.glob("**/*.yml")):
+        with codecs.open(str(yamlfile), encoding="utf-8") as templatefile:
+            try:
+                template = ordered_load(templatefile.read())
+            except yaml.parser.ParserError as error:
+                logger.warning(f"Failed to load {yamlfile.name} template:\n{error}")
                 continue
 
-            with codecs.open(
-                os.path.join(path, name), encoding="utf-8"
-            ) as template_file:
-                try:
-                    template = ordered_load(template_file.read())
-                except yaml.parser.ParserError as error:
-                    logger.warning(f"Failed to load {name} template:\n{error}")
-                    continue
+        template["template_name"] = yamlfile.name
 
-            template["template_name"] = name
+        # Test if all required fields are in template
+        if "keywords" not in template.keys():
+            raise ValueError("Missing mandatory 'keywords' field.")
 
-            # Test if all required fields are in template
-            if "keywords" not in template.keys():
-                raise ValueError("Missing mandatory 'keywords' field.")
+        # Convert keywords to list, if only one
+        if not isinstance(template["keywords"], list):
+            template["keywords"] = [template["keywords"]]
 
-            # Convert keywords to list, if only one
-            if not isinstance(template["keywords"], list):
-                template["keywords"] = [template["keywords"]]
+        # Set excluded_keywords as empty list, if not provided
+        if "exclude_keywords" not in template.keys():
+            template["exclude_keywords"] = []
 
-            # Set excluded_keywords as empty list, if not provided
-            if "exclude_keywords" not in template.keys():
-                template["exclude_keywords"] = []
+        # Convert excluded_keywords to list, if only one
+        if not isinstance(template["exclude_keywords"], list):
+            template["exclude_keywords"] = [template["exclude_keywords"]]
 
-            # Convert excluded_keywords to list, if only one
-            if not isinstance(template["exclude_keywords"], list):
-                template["exclude_keywords"] = [template["exclude_keywords"]]
-
-            invoicetemplates.append(InvoiceTemplate(template))
+        invoicetemplates.append(InvoiceTemplate(template))
 
     logger.info(f"Loaded {len(invoicetemplates)} templates from {folder}")
 
