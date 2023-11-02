invoice-x/invoice2data

commit 0d2d16ca46cb4f28b231b473529d2acd882759f2

Native JSON Template Support

src/invoice2data/extract/loader.py
@@ -1,10 +1,11 @@
 """
 This module abstracts templates for invoice providers.
 
-Templates are initially read from .yml files and then kept as class.
+Templates are initially read from .yml or .json files and then kept as class.
 """
 
 import os
+import json
 try:
     from yaml import load, YAMLError, CSafeLoader as SafeLoader
 except ImportError:  # pragma: no cover
@@ -71,7 +72,11 @@ def read_templates(folder=None):
                     except YAMLError as error:
                         logger.warning("Failed to load %s template:\n%s", name, error)
                         continue
-
+                else:
+                    try:
+                        tpl = json.loads(template_file.read())
+                    except ValueError as error:
+                        logger.warning("json Loader Failed to load %s template:\n%s", name, error)
             tpl["template_name"] = name
 
             # Test if all required fields are in template:

src/invoice2data/extract/templates/com/com.flipkart.WSRetail.yml
@@ -1,15 +1,0 @@
-issuer: Flipkart
-fields:
-  amount: GrandTotal(\d+\.\d+)
-  date: InvoiceDate:(\d{1,4}\-\d{1,2}\-\d{1,4})
-  invoice_number: InvoiceNo:(\S+)
-  order_id: OrderID:(\w{2}\d{16,18})
-keywords:
-- flipkart
-- WS\s?Retail
-- OD
-
-
-options:
-  currency: INR
-  remove_whitespace: true

commit edf31837078dee4113775fd3108c1a5b4fa15d67

loader: require .json extension for JSON files
Trying to parse every file other than *.yml as JSON file isn't the best
idea. It wastes time on trying to parse non-template files. It produces
warnings like "json Loader Failed to load (...)template" for files that
were never meant to be templates. It's a bad pracice in general.

Check for file extension before using json.loads().

Fixes: 0d2d16ca46cb ("Native JSON Template Support")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

src/invoice2data/extract/loader.py
@@ -72,12 +72,14 @@ def read_templates(folder=None):
                     except YAMLError as error:
                         logger.warning("Failed to load %s template:\n%s", name, error)
                         continue
-                else:
+                elif name.endswith(".json"):
                     try:
                         tpl = json.loads(template_file.read())
                     except ValueError as error:
                         logger.warning("json Loader Failed to load %s template:\n%s", name, error)
                         continue
+                else:
+                    continue
             tpl["template_name"] = name
 
             # Test if all required fields are in template