23a880d6acad8655a59595a24d9bb3ec595d7404
bosd <evbokhoven@gmail.com>
2023-11-26 21:35:19 +0100

    Move to PEP518 compliant packaging

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    include_package_data=True,
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    license='MIT License',
-    package_data={
-        'invoice2data.extract': [
-            'templates/com/*.yml',
-            'templates/de/*.yml',
-            'templates/es/*.yml',
-            'templates/fr/*.yml',
-            'templates/nl/*.yml',
-            'templates/ch/*.yml'],
-    },
-)
+setup()
