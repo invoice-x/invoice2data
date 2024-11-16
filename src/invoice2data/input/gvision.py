7acf78ae908d5cda3621ca9d4c89b449d50eedfc
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-11-16 23:49:37 +0100

    Refactor Google Vision, Update api version
    Removed unnecessary imports: Removed the unused json_format import.
    Updated API calls:
    Changed vision.types.Feature to vision.Feature.
    Changed vision.enums.Feature.Type.DOCUMENT_TEXT_DETECTION to vision.Feature.Type.DOCUMENT_TEXT_DETECTION.
    Changed vision.types.GcsSource to vision.GcsSource.
    Changed vision.types.InputConfig to vision.InputConfig.
    Changed vision.types.GcsDestination to vision.GcsDestination.
    Changed vision.types.OutputConfig to vision.OutputConfig.
    Changed vision.types.AsyncAnnotateFileRequest to vision.AsyncAnnotateFileRequest.
    Changed json_format.Parse(json_string, vision.types.AnnotateFileResponse()) to vision.AnnotateFileResponse.from_json(json_string).
    Added type hints: Added type hints to the function parameters and return value.
    Improved docstrings: Added a Google-style docstring to the function.
    Minor code style improvements: Used f-strings for string formatting and improved variable naming consistency.

--- src/invoice2data/input/gvision.py
+++ src/invoice2data/input/gvision.py
@@ -1,69 +1,62 @@
-def to_text(path, bucket_name="cloud-vision-84893", language="en"):
+import os
+
+from google.cloud import storage
+from google.cloud import vision
+
+
+def to_text(
+    path: str, bucket_name: str = "cloud-vision-84893", language: str = "en"
+) -> str:
     """Sends PDF files to Google Cloud Vision for OCR.
 
-    Before using invoice2data, make sure you have the auth json path set as
-    env var GOOGLE_APPLICATION_CREDENTIALS
+    Before using invoice2data, make sure you have the auth JSON path set as
+    the environment variable GOOGLE_APPLICATION_CREDENTIALS.
 
-    Parameters
-    ----------
-    path : str
-        path of electronic invoice in JPG or PNG format
-    bucket_name : str
-        name of bucket to use for file storage and results cache.
+    Args:
+        path (str): Path of the electronic invoice in JPG or PNG format.
+        bucket_name (str, optional): Name of the bucket to use for file storage
+                                     and results cache. Defaults to "cloud-vision-84893".
+        language (str, optional): Language to use for OCR. Defaults to "en".
 
     Returns:
-    -------
-    extracted_str : str
-        returns extracted text from image in JPG or PNG format
-
+        str: Extracted text from the image in JPG or PNG format.
     """
-    """OCR with PDF/TIFF as source files on GCS"""
-    import os
-
-    from google.cloud import storage
-    from google.cloud import vision
-    from google.protobuf import json_format
-
     # Supported mime_types are: 'application/pdf' and 'image/tiff'
     mime_type = "application/pdf"
 
     path_dir, filename = os.path.split(path)
     result_blob_basename = filename.replace(".pdf", "").replace(".PDF", "")
-    result_blob_name = result_blob_basename + "/output-1-to-1.json"
+    result_blob_name = f"{result_blob_basename}/output-1-to-1.json"
     result_blob_uri = f"gs://{bucket_name}/{result_blob_basename}/"
     input_blob_uri = f"gs://{bucket_name}/{filename}"
 
-    # Upload file to gcloud if it doesn't exist yet
+    # Upload file to Google Cloud Storage if it doesn't exist yet
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
-    if bucket.get_blob(filename) is None:
+    if not bucket.exists(filename):
         blob = bucket.blob(filename)
         blob.upload_from_filename(path)
 
-    # See if result already exists
+    # See if the result already exists
     # TODO: upload as hash, not filename
     result_blob = bucket.get_blob(result_blob_name)
     if result_blob is None:
-        # How many pages should be grouped into each json output file.
+        # How many pages should be grouped into each JSON output file.
         batch_size = 10
 
         client = vision.ImageAnnotatorClient()
 
-        feature = vision.types.Feature(
-            type=vision.enums.Feature.Type.DOCUMENT_TEXT_DETECTION
-        )
+        feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
 
-        gcs_source = vision.types.GcsSource(uri=input_blob_uri)
-        input_config = vision.types.InputConfig(
-            gcs_source=gcs_source, mime_type=mime_type
-        )
+        gcs_source = vision.GcsSource(uri=input_blob_uri)
+        input_config = vision.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
 
-        gcs_destination = vision.types.GcsDestination(uri=result_blob_uri)
-        output_config = vision.types.OutputConfig(
+        gcs_destination = vision.GcsDestination(uri=result_blob_uri)
+        output_config = vision.OutputConfig(
             gcs_destination=gcs_destination, batch_size=batch_size
         )
 
-        async_request = vision.types.AsyncAnnotateFileRequest(
+        async_request = vision.AsyncAnnotateFileRequest(
             features=[feature], input_config=input_config, output_config=output_config
         )
 
@@ -76,7 +69,7 @@ def to_text(path, bucket_name="cloud-vision-84893", language="en"):
     result_blob = bucket.get_blob(result_blob_name)
 
     json_string = result_blob.download_as_string()
-    response = json_format.Parse(json_string, vision.types.AnnotateFileResponse())
+    response = vision.AnnotateFileResponse.from_json(json_string)
 
     # The actual response for the first page of the input file.
     first_page_response = response.responses[0]
