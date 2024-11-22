164325c45266607c513b7bc6ae4402a5d33dcfdc
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2024-11-22 20:28:33 +0100

    Refactor: Convert CLI to use Click
    This commit refactors the command-line interface (CLI) to use the `click` library instead of `argparse`.
    
    The `click` library provides a more concise and readable way to define command-line options and arguments. It also offers features like automatic help generation and type validation, improving the user experience.
    
    This change removes the dependency on `argparse` and modernizes the CLI implementation.

--- src/invoice2data/__main__.py
+++ src/invoice2data/__main__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 """Command-line interface."""
 
-import argparse
-import copy
 import datetime
 import logging
 import os
 import shutil
+from copy import deepcopy
 from os.path import join
+from typing import Any
+from typing import ClassVar
+from typing import Dict
+from typing import List
+from typing import Union
 
 import click
 
@@ -45,16 +49,6 @@ output_mapping = {
 }
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """Invoice2Data."""
-
-
-if __name__ == "__main__":
-    main(prog_name="invoice2data")  # pragma: no cover
-
-
 class Color:
     """A class for terminal color codes."""
 
@@ -75,7 +69,7 @@ class ColorLogFormatter(logging.Formatter):
         "%(prefix)s%(levelname)s:%(suffix)s%(name)s:%(prefix)s %(message)s%(suffix)s"
     )
 
-    LOG_LEVEL_COLOR = {
+    LOG_LEVEL_COLOR: ClassVar = {  # Annotate with ClassVar
         "DEBUG": {"prefix": "", "suffix": Color.END},
         "INFO": {"prefix": Color.BLUE, "suffix": Color.END},
         "WARNING": {"prefix": Color.YELLOW_BACK, "suffix": Color.END},
@@ -84,8 +78,11 @@ class ColorLogFormatter(logging.Formatter):
     }
 
     def format(self, record):
-        """Format log records with a default prefix and suffix
-        to terminal color codes that corresponds to the log level name.
+        """Format log and console records with colors.
+
+        Format log records with a default prefix and suffix
+        to terminal color codes that corresponds
+        to the log level name.
         """
         if not hasattr(record, "prefix"):
             record.prefix = self.LOG_LEVEL_COLOR.get(record.levelname.upper()).get(
@@ -109,46 +106,44 @@ if not logger.handlers:
     logger.addHandler(stream_handler)
 
 
-def extract_data(invoicefile, templates=None, input_module=None):
+def extract_data(
+    invoicefile: str, templates: List[Any] = None, input_module: Any = None
+) -> Union[Dict, bool]:
     """Extracts structured data from PDF/image invoices.
 
     This function uses the text extracted from a PDF file or image and
     pre-defined regex templates to find structured data.
 
-    Reads template if no template assigned
-    Required fields are matches from templates
+    Reads template if no template assigned.
+    Required fields are matches from templates.
 
-    Parameters
-    ----------
-    invoicefile : str
-        path of electronic invoice file in PDF,JPEG,PNG (example: "/home/duskybomb/pdf/invoice.pdf")
-    templates : list of instances of class `InvoiceTemplate`, optional
-        Templates are loaded using `read_template` function in `loader.py`
-    input_module : {'pdftotext', 'pdfminer', 'tesseract', 'text'}, optional
-        library to be used to extract text from given `invoicefile`,
+
+    Args:
+        invoicefile (str): Path of electronic invoice file in PDF, JPEG, PNG
+                            (example: "/home/duskybomb/pdf/invoice.pdf").
+        templates (List[Any], optional): List of instances of class `InvoiceTemplate`.
+                                    Templates are loaded using `read_template` function in `loader.py`.
+        input_module (Any, optional): Library to be used to extract text
+                                         from the given `invoicefile`.
+                                         Choices: {'pdftotext', 'pdfminer', 'tesseract', 'text'}.
 
     Returns:
-    -------
-    dict or False
-        extracted and matched fields or False if no template matches
+        Union[Dict, bool]: Extracted and matched fields, or False if no template matches.
 
     Notes:
-    -----
-    Import required `input_module` when using invoice2data as a library
+        Import the required `input_module` when using invoice2data as a library.
 
     See Also:
-    --------
-    read_template : Function where templates are loaded
-    InvoiceTemplate : Class representing single template files that live as .yml files on the disk
+        read_template: Function to load templates.
+        InvoiceTemplate: Class representing single template files that live as .yml files on the disk.
 
     Examples:
-    --------
-    When using `invoice2data` as an library
+        When using `invoice2data` as a library:
 
-    >>> from invoice2data.input import pdftotext
-    >>> extract_data("invoice2data/test/pdfs/oyo.pdf", None, pdftotext)
-    {'issuer': 'OYO', 'amount': 1939.0, 'date': datetime.datetime(2017, 12, 31, 0, 0), 'invoice_number': 'IBZY2087',
-     'currency': 'INR', 'desc': 'Invoice IBZY2087 from OYO'}
+        >>> from invoice2data.input import pdftotext
+        >>> extract_data("invoice2data/test/pdfs/oyo.pdf", None, pdftotext)
+        {'issuer': 'OYO', 'amount': 1939.0, 'date': datetime.datetime(2017, 12, 31, 0, 0), 'invoice_number': 'IBZY2087',
+        'currency': 'INR', 'desc': 'Invoice IBZY2087 from OYO'}
 
     """
     if input_module is None:
@@ -181,7 +176,7 @@ def extract_data(invoicefile, templates=None, input_module=None):
         if ocrmypdf.have_ocrmypdf() and input_module is not ocrmypdf:
             logger.debug("Text extraction failed, falling back to ocrmypdf")
             extracted_str, invoicefile, templates_matched = (
-                extract_data_fallback_ocrmypdf(invoicefile, templates)
+                extract_data_fallback_ocrmypdf(invoicefile, templates, input_module)
             )
             if not templates_matched:
                 logger.error("No template for %s", invoicefile)
@@ -193,162 +188,166 @@ def extract_data(invoicefile, templates=None, input_module=None):
     t = templates_matched[0]
     logger.info("Using %s template", t["template_name"])
     optimized_str = t.prepare_input(extracted_str)
-    return t.extract(optimized_str, invoicefile, input_module)
+
+    result = t.extract(optimized_str, invoicefile, input_module)
+
+    # Check if the result is a dictionary before deepcopy
+    if isinstance(result, dict):
+        return deepcopy(result)
+    else:
+        logger.warning(
+            "Extraction result is not a dictionary for %s. " "Skipping deepcopy.",
+            invoicefile,
+        )
+        return result  # Return the original result without deepcopy
 
 
-def extract_data_fallback_ocrmypdf(invoicefile, templates):
-    logger.debug("Text extraction failed, falling back to ocrmypdf")
+def extract_data_fallback_ocrmypdf(
+    invoicefile, templates, input_module
+):  # Add input_module parameter
+    logger.debug("Trying OCR extraction with ocrmypdf")
     extracted_str = ocrmypdf.to_text(invoicefile)
     templates_matched = filter(lambda t: t.matches_input(extracted_str), templates)
     templates_matched = sorted(
         templates_matched, key=lambda k: k["priority"], reverse=True
     )
-    return extracted_str, invoicefile, templates_matched
+
+    if templates_matched and isinstance(
+        templates_matched[0].extract(extracted_str, invoicefile, ocrmypdf), dict
+    ):
+        result = templates_matched[0].extract(extracted_str, invoicefile, ocrmypdf)
+        if not isinstance(result, dict):
+            logger.warning(
+                "OCR result is not a dictionary for %s. Skipping deepcopy.",
+                invoicefile,
+            )
+            return extracted_str, invoicefile, templates_matched
+        return extracted_str, invoicefile, [deepcopy(templates_matched[0])]
 
 
-def create_parser():
-    """Returns argument parser."""
-    parser = argparse.ArgumentParser(
-        description="Extract structured data from PDF files and save to CSV or JSON."
-    )
-
-    parser.add_argument(
-        "--input-reader",
-        choices=input_mapping.keys(),
-        help="Choose text extraction function. Default: auto-detect between text & pdftotext",
-    )
-
-    parser.add_argument(
-        "--output-format",
-        choices=output_mapping.keys(),
-        default="none",
-        help="Choose output format. Default: none",
-    )
-
-    parser.add_argument(
-        "--output-date-format",
-        dest="output_date_format",
-        default="%Y-%m-%d",
-        help="Choose output date format. Default: %%Y-%%m-%%d (ISO 8601 Date)",
-    )
-
-    parser.add_argument(
-        "--output-name",
-        "-o",
-        dest="output_name",
-        default="invoices-output",
-        help="Custom name for output file. Extension is added based on chosen format.",
-    )
-
-    parser.add_argument(
-        "--debug", dest="debug", action="store_true", help="Enable debug information."
-    )
-
-    parser.add_argument(
-        "--copy",
-        "-c",
-        dest="copy",
-        help="Copy and rename processed PDFs to specified folder.",
-    )
-
-    parser.add_argument(
-        "--move",
-        "-m",
-        dest="move",
-        help="Move and rename processed PDFs to specified folder.",
-    )
-
-    parser.add_argument(
-        "--filename-format",
-        dest="filename",
-        default="{date} {invoice_number} {desc}.pdf",
-        help="Filename format to use when moving or copying processed PDFs."
-        'Default: "{date} {invoice_number} {desc}.pdf"',
-    )
-
-    parser.add_argument(
-        "--template-folder",
-        "-t",
-        dest="template_folder",
-        help="Folder containing invoice templates in yml file. Always adds built-in templates.",
-    )
-
-    parser.add_argument(
-        "--exclude-built-in-templates",
-        dest="exclude_built_in_templates",
-        default=False,
-        help="Ignore built-in templates.",
-        action="store_true",
-    )
-
-    parser.add_argument(
-        "input_files",
-        type=argparse.FileType("r"),
-        nargs="+",
-        help="File or directory to analyze.",
-    )
-
-    return parser
-
-
-def main(args=None):
-    """Take folder or single file and analyze each."""
-    if args is None:
-        parser = create_parser()
-        args = parser.parse_args()
-
-    if args.debug:
+@click.command()
+@click.option(
+    "--input-reader",
+    "-i",
+    type=click.Choice(input_mapping.keys()),
+    # default="pdftotext", # bosd
+    help="Choose text extraction function. Default: auto-detect between text & pdftotext",
+)
+@click.option(
+    "--output-format",
+    "-f",
+    type=click.Choice(output_mapping.keys()),
+    default="none",
+    help="Choose output format. Default: none",
+)
+@click.option(
+    "--output-date-format",
+    "-d",
+    default="%Y-%m-%d",
+    help="Choose output date format. Default: %%Y-%%m-%%d (ISO 8601 Date)",
+)
+@click.option(
+    "--output-name",
+    "-o",
+    default="invoices-output",
+    help="Custom name for output file. Extension is added based on chosen format.",
+)
+@click.option("--debug", is_flag=True, help="Enable debug information.")
+@click.option(
+    "--copy", "-c", help="Copy and rename processed PDFs to specified folder."
+)
+@click.option(
+    "--move", "-m", help="Move and rename processed PDFs to specified folder."
+)
+@click.option(
+    "--filename-format",
+    default="{date} {invoice_number} {desc}.pdf",
+    help="Filename format to use when moving or copying processed PDFs."
+    'Default: "{date} {invoice_number} {desc}.pdf"',
+)
+@click.option(
+    "--template-folder",
+    "-t",
+    type=click.Path(exists=True),
+    help="Folder containing invoice templates in yml file. Always adds built-in templates.",
+)
+@click.option(
+    "--exclude-built-in-templates",
+    is_flag=True,
+    help="Ignore built-in templates.",
+)
+@click.argument(
+    "input_files",
+    type=click.File("wb"),
+    # type=click.Path(exists=True),
+    nargs=-1,
+)
+# @click.pass_context
+@click.version_option()
+def main(
+    input_reader,
+    output_format,
+    output_date_format,
+    output_name,
+    debug,
+    copy,
+    move,
+    filename_format,
+    template_folder,
+    exclude_built_in_templates,
+    input_files,
+):
+    """Extract structured data from PDF files and save to CSV or JSON."""
+    if debug:
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(level=logging.INFO)
 
-    input_module = (
-        input_mapping[args.input_reader] if args.input_reader is not None else None
-    )
-    output_module = output_mapping[args.output_format]
+    input_module = input_mapping.get(input_reader)
+    output_module = output_mapping[output_format]
 
     templates = []
+    if template_folder:
+        templates.extend(read_templates(os.path.abspath(template_folder)))
+    if not exclude_built_in_templates:
+        templates.extend(read_templates())
 
-    # Load templates from external folder if set.
-
-    if args.template_folder:
-        templates += read_templates(os.path.abspath(args.template_folder))
-
-    # Load internal templates, if not disabled.
-
-    if not args.exclude_built_in_templates:
-        templates += read_templates()
     output = []
-    for f in args.input_files:
-        try:
+    for f in input_files:
+        try:  # Check if res is not None
             res = extract_data(f.name, templates=templates, input_module=input_module)
             if res:
                 logger.info(res)
                 output.append(res)
 
-                kwargs = copy.deepcopy(res)
-                for key, value in kwargs.items():
-                    if type(value) is list and len(value) >= 1:
-                        kwargs[key] = value[0]
-                for key, value in kwargs.items():
-                    if type(value) is datetime.datetime:
-                        kwargs[key] = value.strftime("%Y-%m-%d")
-                if args.copy:
-                    filename = args.filename.format(**kwargs)
-                    shutil.copyfile(f.name, join(args.copy, filename))
-                if args.move:
-                    filename = args.filename.format(**kwargs)
-                    shutil.move(f.name, join(args.move, filename))
-            f.close()
+                if copy or move:  # Only perform copy/move operations if needed
+                    # Create a deepcopy of res only if it's a dictionary
+                    if not isinstance(res, dict):
+                        kwargs = res.to_dict()  # Convert KeyValue to dict
+                    else:
+                        kwargs = deepcopy(res)
+                    for key, value in kwargs.items():
+                        if isinstance(value, list) and len(value) >= 1:
+                            kwargs[key] = value[0]
+                    for key, value in kwargs.items():
+                        if isinstance(value, datetime.datetime):
+                            kwargs[key] = value.strftime("%Y-%m-%d")
+                    if copy:
+                        filename = filename_format.format(**kwargs)
+                        shutil.copyfile(f.name, join(copy, filename))
+                    if move:
+                        filename = filename_format.format(**kwargs)
+                        shutil.move(f.name, join(move, filename))
         except Exception as e:
             logger.critical(
                 "Invoice2data failed to process %s. \nError message: %s", f.name, e
             )
-            continue
+        finally:
+            f.close()
 
     if output_module is not None:
-        output_module.write_to_file(output, args.output_name, args.output_date_format)
+        output_module.write_to_file(output, output_name, output_date_format)
 
 
 if __name__ == "__main__":
-    main()
+    main(prog_name="invoice2data")  # pragma: no cover
